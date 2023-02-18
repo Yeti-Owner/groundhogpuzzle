7d9fa5ca4adb46c471709b81261c9621dd2d92b8
Callum <calrossmaclean@gmail.com>
2023-02-18 08:33:18 -0500

    Work on Menu and Journal

--- scripts/ui/gui.gd
+++ scripts/ui/gui.gd
@@ -2,9 +2,18 @@ extends CanvasLayer
 
 @onready var InteractionLabel := $InteractionText
 @onready var InteractIcon := $CenterContainer/crosshair
+@onready var Clock := $TextureRect/Label
+@onready var Journal := $Journal
+
+var TimeLeft:int = 120
 
 func _ready():
 	Eventbus.interaction.connect(_set_interaction)
+	Journal.visible = false
+
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("journal"):
+		Journal.visible = !Journal.visible
 
 func _set_interaction(icon, text):
 	if icon == null:
@@ -18,3 +27,17 @@ func _set_interaction(icon, text):
 	else:
 		InteractionLabel.set_text(text)
 		InteractionLabel.set_visible(true)
+
+func _on_timer_timeout():
+	# Update the time left and the label text
+	TimeLeft -= 1
+	var minutes = int(TimeLeft / 60)
+	var seconds = TimeLeft % 60
+	var seconds_string = str(seconds)
+	if seconds < 10:
+		seconds_string = "0" + seconds_string
+	Clock.text = str(minutes) + ":" + seconds_string
+	
+	# Stop the timer when the time is up
+	if TimeLeft == 0:
+		$Timer.stop()
---
+++ scripts/ui/menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var title := $Title
+@onready var vboxoptions := $VBoxContainer
+@onready var InfoPopup := $InfoPopup
+
+func _ready():
+	title.position = Vector2(-300, 270)
+	vboxoptions.position = Vector2(-300, 179)
+	InfoPopup.visible = false
+	_intro_anim()
+
+func _intro_anim():
+	var tween := get_tree().create_tween()
+	tween.tween_property(title, "position", Vector2(0, 270), 2).set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(vboxoptions, "position", Vector2(11, 179), 2).set_trans(Tween.TRANS_ELASTIC)
+
+
+func _on_info_pressed():
+	InfoPopup.visible = !InfoPopup.visible
+
+func _on_info_close_pressed():
+	InfoPopup.visible = false
