f36ed7055bc84ac4bd9ecb52b8cf99fc99cdb158
Callum <calrossmaclean@gmail.com>
2023-03-08 12:53:57 -0500

    Added second hint
    and fully fleshed out page system.

--- scenes/UI/Journal.gd
+++ scenes/UI/Journal.gd
@@ -15,17 +15,25 @@ func _next_page():
 	_update_journal()
 
 func _last_page():
-	CurrentPage = max(CurrentPage - 1, -1)
+	CurrentPage = max(CurrentPage - 1, 0)
 	_update_journal()
 
 func _update_journal():
+	# Remove current page + text
+	for child in Left.get_children():
+		child.queue_free()
+	RightText.text = ""
+	
+	# Add new page + text
 	match Eventbus.JournalEntries[CurrentPage]:
 		"journal":
-			var page:PackedScene = load("res://scenes/journalpages/page_1.tscn")
-			var _page:Node = page.instantiate()
-			Left.add_child(_page)
-			RightText.text = "- Time machine trial #09124\n- Subject's vitals seem alright ig"
+			_set_page(load("res://scenes/journalpages/page_1.tscn"), "- Time machine trial #09124\n- Subject's vitals seem alright ig")
 		"audio":
 			pass
-	
-	
+		"note":
+			_set_page(load("res://scenes/journalpages/page_stickynote.tscn"), "- This is text 2\n - Yup")
+
+func _set_page(pagescene:PackedScene, text:String):
+	var _page:Node = pagescene.instantiate()
+	Left.add_child(_page)
+	RightText.text = text
