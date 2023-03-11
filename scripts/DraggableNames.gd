1f1bd52faf9646558c3c12f1d9149e7ea4703054
Callum <calrossmaclean@gmail.com>
2023-03-11 12:53:46 -0500

    Finalized Polling
    Only need to add a check/save so players can only send once.

--- scripts/DraggableNames.gd
+++ scripts/DraggableNames.gd
@@ -1,23 +1,16 @@
 extends Label
 
-var dragging = false
+@onready var Owner := get_parent().get_parent().get_parent()
 
-func _on_label_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		dragging = true
-#		set_drag_preview(get_texture().get_data())
+func _get_drag_data(_at_position):
+	Owner.LastNode = self.get_path()
+#	print(str(self.name) + " get")
+	return self.text
 
-func _on_label_gui_input_event(event):
-	if dragging:
-#		set_drag_data("label_index", get_index())
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+func _can_drop_data(at_position, data):
+	return true
 
-func _on_input_event(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed and event.get_drag_data():
-		var drag_data = event.get_drag_data()
-		var label_index = drag_data.get_data()
-		var target_index = get_index()
-		if label_index != target_index:
-			get_parent().move_child(get_node(str(label_index)), target_index)
-		dragging = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+func _drop_data(_at_position, data):
+	get_node(Owner.LastNode).text = text
+#	print(str(self.name) + " drop")
+	text = data
--- scripts/ui/poll.gd
+++ scripts/ui/poll.gd
@@ -1,14 +1,28 @@
 extends Control
 
-var url = "https://eoc51iyh2mrie8h.m.pipedream.net"
-var data_to_send = {'string': "test1", 'integer': 1}
+var LastNode
 
-func _on_button_pressed():
-	_make_post_request(url, data_to_send)
+#var uid := 867142
+var url := "https://eoc51iyh2mrie8h.m.pipedream.net"
+#var data_to_send := {'string': "test1", 'integer': 1}
 
-func _make_post_request(_url, _data_to_send):
+func _make_post_request(_url, data_to_send):
 	# Convert data to json string:
-	var query = JSON.stringify(_data_to_send)
+	var query := JSON.stringify(data_to_send)
 	# Add 'Content-Type' header:
-	var headers = ["Content-Type: application/json"]
+	var headers := ["Content-Type: application/json"]
 	$HTTPRequest.request(_url, headers, HTTPClient.METHOD_POST, query)
+
+func _on_submit_pressed():
+	@warning_ignore("unassigned_variable")
+	var Order:Dictionary
+	var i:int = 0
+	
+	for child in $HBoxContainer/VBoxContainer2.get_children():
+		if child.get_class() == "Label":
+			i += 1
+			Order[i] = child.text
+	
+#	print(Order)
+	
+	_make_post_request(url, Order)
